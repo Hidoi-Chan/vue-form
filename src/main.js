d42bd9d92b2f96405640edf8e0627c4fec007eae
Hidoi-Chan <HidoiChan@mail.ru>
2021-03-30 23:02:59 +0300

    deploy

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-Vue.config.devtools = true
-
-new Vue({
-  render: function (h) { return h(App) },
-}).$mount('#app')
